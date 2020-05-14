8d2cba4bf6a4cf378d2818845a9cfddaefda9b1d
Thamizhathena <42510520+Thamizhathena@users.noreply.github.com>
2020-05-15 04:11:29 +0530

    Update app.py

--- clock/app.py
+++ clock/app.py
@@ -30,4 +30,4 @@ def home():
     return render_template("Home.html", pageData=str(answer))
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
