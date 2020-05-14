2939cfb28532be624e84cd0fa3f211f290f6289d
Thamizhathena <42510520+Thamizhathena@users.noreply.github.com>
2020-05-15 05:04:02 +0530

    Update app.py

--- clock/app.py
+++ clock/app.py
@@ -30,5 +30,5 @@ def home():
     return render_template("Home.html", pageData=str(answer))
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))    
+    app.run(debug=True, host='0.0.0.0', port = int(os.environ.get('PORT', 8080)))    
 
