ab5b5c962e74d87c131a104438c3919e963392b2
Thamizhathena <42510520+Thamizhathena@users.noreply.github.com>
2020-05-15 04:51:11 +0530

    add port new

--- clock/app.py
+++ clock/app.py
@@ -30,4 +30,5 @@ def home():
     return render_template("Home.html", pageData=str(answer))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))    
+
