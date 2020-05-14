37dd3b9eee5fd483b87b414a28c68bd698974b73
Thamizhathena <42510520+Thamizhathena@users.noreply.github.com>
2020-05-15 02:51:59 +0530

    Update app.py

--- clock/app.py
+++ clock/app.py
@@ -1,5 +1,5 @@
 """
-high level support for doing this and that.
+code for calculate angle
 """
 from flask import Flask, render_template, request
 from clock import Clock_Angle
@@ -8,25 +8,25 @@ app = Flask(__name__)
 @app.after_request
 def add_header(newr):
     """
-    high level support for doing this and that.
+    to add header
     """
     newr.headers["Pragma"] = "no=cache"
     newr.headers["Expires"] = "0"
     newr.headers["Cache-Control"] = "public, max-age=0"
     return newr
 
-@app.route("/home", methods=["GET","POST"])
-@app.route("/", methods=["GET","POST"])
+@app.route("/home", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 
 def home():
     """
-    high level support for doing this and that.
+    home to calculate angle
     """
     answer = ""
     if "fHour" in request.form and "fMins" in request.form:
         hour = float(request.form["fHour"])
-        minutes = float(request.form["fMins"])                 
-        answer = Clock_Angle(hour,minutes).calAngle()
+        minutes = float(request.form["fMins"])
+        answer = Clock_Angle(hour, minutes).calAngle()
     return render_template("Home.html", pageData=str(answer))
 
 if __name__ == "__main__":
