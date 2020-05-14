86a053934f8de0e7339b96c4a5fceb0d9dff2b38
Thamizhathena <42510520+Thamizhathena@users.noreply.github.com>
2020-05-15 02:45:35 +0530

    cahnges pylint

--- clock/app.py
+++ clock/app.py
@@ -1,24 +1,33 @@
-from flask import Flask,render_template,json,request
+"""
+high level support for doing this and that.
+"""
+from flask import Flask, render_template, request
 from clock import Clock_Angle
 app = Flask(__name__)
 
 @app.after_request
-def add_header(r):
-    r.headers["Pragma"] = "no=cache"
-    r.headers["Expires"]="0"
-    r.headers["Cache-Control"] = "public, max-age=0"
-    return r
+def add_header(newr):
+    """
+    high level support for doing this and that.
+    """
+    newr.headers["Pragma"] = "no=cache"
+    newr.headers["Expires"] = "0"
+    newr.headers["Cache-Control"] = "public, max-age=0"
+    return newr
 
+@app.route("/home", methods=["GET","POST"])
+@app.route("/", methods=["GET","POST"])
 
-
-@app.route("/home",methods=["GET","POST"])
-@app.route("/",methods=["GET","POST"])
 def home():
+    """
+    high level support for doing this and that.
+    """
     answer = ""
-    if "fHour" in request.form and "fMins" in request.form:        
-        answer = str(Clock_Angle(float(request.form["fHour"]),float(request.form["fMins"])).calAngle())
-        print(answer)
-    return render_template("Home.html",pageData=str(answer))
+    if "fHour" in request.form and "fMins" in request.form:
+        hour = float(request.form["fHour"])
+        minutes = float(request.form["fMins"])                 
+        answer = Clock_Angle(hour,minutes).calAngle()
+    return render_template("Home.html", pageData=str(answer))
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(port=80)
